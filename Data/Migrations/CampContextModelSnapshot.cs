02725bec3f6fc101ef5ed84fc8ca6aac4afe4130
enriqueescobar-askida <enrique.escobar@askida.com>
2019-02-09 17:49:43 -0500

    db stuff

--- Data/Migrations/CampContextModelSnapshot.cs
+++ Data/Migrations/CampContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace CoreCodeCamp.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.0-preview2-35157")
+                .HasAnnotation("ProductVersion", "2.2.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
